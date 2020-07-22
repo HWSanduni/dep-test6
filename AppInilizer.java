8a0b44b684d5b70c303084f9a0adfc027c0323af
HWSanduni <sandushashikala96@gmail.com>
2020-07-22 11:16:04 +0530

    Initial commit

---
+++ AppInilizer.java
@@ -0,0 +1,9 @@
+class AppInilizer{
+    public static void main (String args []){
+        Student s1 = new Student(1,"sandu");
+        System.out.print(s1);
+
+         Student student1 = new Student (2,"ruwani");
+         System.out.print(student1);
+    }
+}
---
+++ Student.java
@@ -0,0 +1,15 @@
+class Student {
+    int id;
+    String name;
+
+ Student(){
+     
+ }
+
+    public Student (int id,String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    
+}
